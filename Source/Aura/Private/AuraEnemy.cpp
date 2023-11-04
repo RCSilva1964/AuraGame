d79942f5c8458193f8ad96ac2193fcf655d391e8
RCSilva1964 <robertocostaesilva@hotmail.com>
2023-11-04 09:44:16 -0300

    Commit Inicial

---
+++ Source/Aura/Private/AuraEnemy.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraEnemy.h"
+
+void AAuraEnemy::HighlightActor()
+{
+    bHighlighted = true;
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+    bHighlighted = false;
+}
---
+++ Source/Aura/Public/AuraEnemy.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "EnemyInterface.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
+{
+	GENERATED_BODY()
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bHighlighted = false;
+	
+};
